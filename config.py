670d7643f0727e29b5fb02e403fc8dd11b85ade6
yuyetao <2186853371@qq.com>
2026-06-19 20:09:25 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT = os.path.dirname(os.path.abspath(__file__))
 DATA_ROOT = os.path.join(ROOT, 'data')
 MALE_DIR = os.path.join(DATA_ROOT, 'male')
 FEMALE_DIR = os.path.join(DATA_ROOT, 'female')
